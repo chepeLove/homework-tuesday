35aad8688c52dceba22ca29c62df283bcbca6708
chepeLove <ilya.chepelov@gmail.com>
2023-09-27 14:22:35 +0300

    did HW3

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-// import { v1 } from 'uuid'
-// import s2 from '../../s1-main/App.module.css'
-// import GreetingContainer from './GreetingContainer'
+import React, {useState} from 'react'
+import {v1} from 'uuid'
+import s2 from '../../s1-main/App.module.css'
+import GreetingContainer from './GreetingContainer'
 //
 // /*
 // * 1 - описать тип UserType
@@ -17,38 +17,41 @@ import React, { useState } from 'react'
 // * 11 - сделать стили в соответствии с дизайном
 // * */
 //
-// // types
-// export type UserType = {
-//     _id: any // need to fix any
-//     name: any // need to fix any
-// }
+// types
+export type UserType = {
+    _id: string // need to fix any
+    name: string // need to fix any
+}
 //
-// export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-//     const user = { // need to fix
-//     }
-//     setUsers([...users, user])
-// }
-//
-// const HW3 = () => {
-//     const [users, setUsers] = useState<any>([]) // need to fix any
-//
-//     const addUserCallback = (name: any) => { // need to fix any
-//         pureAddUserCallback(name, setUsers, users)
-//     }
-//
-//     return (
-//         <div id={'hw3'}>
-//             <div className={s2.hwTitle}>Homework #3</div>
-//             {/*для автоматической проверки дз (не менять)*/}
-//
-//             <div className={s2.hw}>
-//                 <GreetingContainer
-//                     users={users}
-//                     addUserCallback={addUserCallback}
-//                 />
-//             </div>
-//         </div>
-//     )
-// }
-//
-// export default HW3
+export const pureAddUserCallback = (name: string, setUsers:(users: UserType[]) =>void, users: UserType[]) => { // need to fix any
+    const user = {
+        _id: v1(),
+        name: name
+    }
+    setUsers([...users, user])
+}
+
+
+const HW3 = () => {
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
+
+    const addUserCallback = (name: string) => { // need to fix any
+        pureAddUserCallback(name, setUsers, users)
+    }
+
+    return (
+        <div id={'hw3'}>
+            <div className={s2.hwTitle}>Homework #3</div>
+            {/*для автоматической проверки дз (не менять)*/}
+
+            <div className={s2.hw}>
+                <GreetingContainer
+                    users={users}
+                    addUserCallback={addUserCallback}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HW3
